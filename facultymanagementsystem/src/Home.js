6545f2636d0090381cba19024f4c3f29589f973f
theamazingbnd <bnd2524@gmail.com>
2018-09-23 00:00:08 -0400

    Just added a logo and started containerizing. (Also probaly ill worded)

---
+++ facultymanagementsystem/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export default function Home(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome</h1>
+      </header>
+      <p className="App-intro">
+      </p>
+    </div>
+  );
+}
