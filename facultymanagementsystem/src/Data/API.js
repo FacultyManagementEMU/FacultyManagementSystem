5ddfb1736c4c79978536056322f38518a18a7fa0
theamazingbnd <bnd2524@gmail.com>
2018-11-11 12:06:45 -0500

    Nuking my trash

--- facultymanagementsystem/src/Data/API.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function getPHPTest(){
-    fetch('http://mortondevsite.com/eval/api/v1/users')
-    .then(res => res.json())
-    .then(body => console.log(body))
-    .catch(err => console.log(err));
-};
